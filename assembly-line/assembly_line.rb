7003e6dd3465108269fdb3f770485bfb778956f0
Bianca Power <bianca@biancapower.com>
2023-05-25 19:48:45 +1000

    Solve assembly-line

--- assembly-line/assembly_line.rb
+++ assembly-line/assembly_line.rb
@@ -1,13 +1,27 @@
 class AssemblyLine
+  # at speed = 1
+  CARS_PER_HOUR = 221
+
   def initialize(speed)
     @speed = speed
+
+    @success_rate = case @speed
+      when 1..4
+        1
+      when 5..8
+        0.9
+      when 9
+        0.8
+      when 10
+        0.77
+      end
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    CARS_PER_HOUR * @speed * @success_rate
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
   end
 end
